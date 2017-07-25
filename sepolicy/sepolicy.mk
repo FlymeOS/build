e73a56eee62e675edd189b269587181f9ebda03d
67891abc <1768300180@qq.com>
2017-07-25 16:28:31 +0800

    build:Replace"mxl"with"xml"

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -21,7 +21,7 @@ mac_permissions: $(mac_perms_keys.tmp) $(OUT_MAC_PERMISSIONS_XML)
 	$(hide) DEFAULT_SYSTEM_DEV_CERTIFICATE=`echo $(PORT_ROOT)/$(CERTS_PATH)` \
 		$(SEPOLICY_DIR)/insertkeys.py $< $(OUT_MAC_PERMISSIONS_XML) -o tmp.xml
 	$(hide) mv tmp.xml $(OUT_MAC_PERMISSIONS_XML)
-	$(hide) echo "<<< generating mac_permissions.mxl done"
+	$(hide) echo "<<< generating mac_permissions.xml done"
 	$(hide) echo "* mac_permissions.xml out ==> $(OUT_MAC_PERMISSIONS_XML)"
 	$(hide) echo " "
 
